f96b9eec9df97030cb2b1af2edfa6f46df7e0328
codewithjo3 <sometimewithjoel@gmail.com>
2021-03-17 14:46:49 +0530

    done

---
+++ resources/views/add.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">Add</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/search">Search</a>
+        </li>
+        
+      </ul>
+     
+    </div>
+  </div>
+</nav>
+<div class="container">
+<div class="row">
+<div class="col col-12 col-sm-3 col-lg-3 col-xl-3 col-xxl-3"></div>
+<div class="col col-12 col-sm-6 col-lg-6 col-xl-6 col-xxl-6">
+<h1><center>ADD EMPLOYEE</center></h1>
+<table class="table">
+<tr>
+    <td>employee name</td>
+    <td><input type="text" class="form-control"></text></td>
+</tr>
+
+<tr>
+    <td>employee code</td>
+    <td><input type="number" class="form-control"></text></td>
+</tr>
+
+<tr>
+    <td>designation</td>
+    <td><input type="text" class="form-control"></text></td>
+</tr>
+
+<tr>
+    <td>salary</td>
+    <td><input type="text" class="form-control"></text></td>
+</tr>
+
+<tr>
+    <td>date of birth</td>
+    <td><input type="date" class="form-control"></text></td>
+</tr>
+
+<tr>
+    <td>place</td>
+    <td><input type="text" class="form-control"></text></td>
+</tr>
+
+<tr>
+    <td>pin code</td>
+    <td><input type="text" class="form-control"></text></td>
+</tr>
+
+<tr>
+    <td>phone number</td>
+    <td><input type="number" class="form-control"></text></td>
+</tr>
+
+</table>
+</div> 
+<div class="col col-12 col-sm-6 col-lg-6 col-xl-6 col-xxl-6"></div>   
+</div>
+</div>
+</body>
+</html>
---
+++ resources/views/search.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">Add</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/search">Search</a>
+        </li>
+        
+      </ul>
+     
+    </div>
+  </div>
+</nav>
+<div class="container">
+<div class="row">
+<div class="col col-12 col-sm-3 col-lg-3 col-xl-3 col-xxl-3"></div>
+<div class="col col-12 col-sm-6 col-lg-6 col-xl-6 col-xxl-6">
+<h1><center>SEARCH EMPLOYEE</center></h1>
+<table class="table">
+
+<tr>
+    <td>code</td>
+    <td><input type="text" class="form-control"></text></td>
+</tr>
+
+<tr>
+    <td></td>
+    <td><button class="btn btn-primary">search</button></text></td>
+</tr>
+
+
+
+</table>
+</div>
+<div class="col col-12 col-sm-6 col-lg-6 col-xl-6 col-xxl-6"></div>
+</div>
+</div>
+</body>
+</html>
