0bf8a3fcf943951e75bd77dc3b20adf992eb1c01
codewithjo3 <sometimewithjoel@gmail.com>
2021-03-17 15:40:56 +0530

    done

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,15 +4,15 @@
 <div class="container">
 <div class="row">
 <div class="col col-12 col-sm-3 col-lg-3 col-xl-3 col-xxl-3">
-<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo numquam magnam placeat, corporis asperiores dolore ratione ea temporibus suscipit ipsam, nisi sunt iste eligendi quos distinctio id! Officia, obcaecati laboriosam!
-lorem</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum dolores totam dolore non quos in sit, aliquid incidunt. Dignissimos illum excepturi vel quam ab soluta nam amet sunt, quas labore.</p>
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo numquam magnam placeat, corporis asperiores dolore ratione ea temporibus suscipit ipsam, nisi sunt iste eligendi quos distinctio id! Officia, obcaecati laboriosam!
+lorem
+
 
 </div>
 <div class="col col-12 col-sm-6 col-lg-6 col-xl-6 col-xxl-6">
-<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo numquam magnam placeat, corporis asperiores dolore ratione ea temporibus suscipit ipsam, nisi sunt iste eligendi quos distinctio id! Officia, obcaecati laboriosam!
-lorem</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum dolores totam dolore non quos in sit, aliquid incidunt. Dignissimos illum excepturi vel quam ab soluta nam amet sunt, quas labore.</p>
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo numquam magnam placeat, corporis asperiores dolore ratione ea temporibus suscipit ipsam, nisi sunt iste eligendi quos distinctio id! Officia, obcaecati laboriosam!
+lorem
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum dolores totam dolore non quos in sit, aliquid incidunt. Dignissimos illum excepturi vel quam ab soluta nam amet sunt, quas labore.
 </div>
 
 @endsection
