f1bf3267b4ae76f79e0b7f0b1117a3a4f33c15e7
codewithjo3 <sometimewithjoel@gmail.com>
2021-03-17 15:00:53 +0530

    done

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
+<body style="background color:cadetblue;">
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="/">Add</a>
